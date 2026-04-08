706ae91e07b729b0c5e4aecb44cee37b4429be82
Oliver Huang <oliver721003@gmail.com>
2026-04-08 17:20:49 +0800

    feat(product): 變更作者資訊為文字陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class App {
       new Product({
         id: 1,
         name: '書籍 A',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -32,7 +32,7 @@ export class App {
       new Product({
         id: 2,
         name: '書籍 B',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -42,7 +42,7 @@ export class App {
       new Product({
         id: 3,
         name: '書籍 C',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -52,7 +52,7 @@ export class App {
       new Product({
         id: 4,
         name: '書籍 D',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -62,7 +62,7 @@ export class App {
       new Product({
         id: 5,
         name: '書籍 E',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
