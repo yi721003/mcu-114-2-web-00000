ae4b437b84cdd5c7334a968b3385a4e46c3856c1
Oliver Huang <oliver721003@gmail.com>
2026-04-08 16:10:46 +0800

    feat(product): 設定產品資訊為輸入型屬性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { ProductCardComponent } from './product-card/product-card.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class App {}
+export class App {
+  protected productName = '書籍 A';
+  protected author = '作者甲、作者乙、作者丙';
+  protected company = '博碩文化';
+
+  protected isShow = true;
+
+  protected photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+}
