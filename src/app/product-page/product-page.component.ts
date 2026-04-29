0274cd1b63a8911a2c41b9efa59bdf089c25590d
Oliver Huang <oliver721003@gmail.com>
2026-04-29 17:06:12 +0800

    feat(product): 實作產品資料分頁作業

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,25 +1,36 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from '../services/product.service';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardListComponent],
+  imports: [PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   private router = inject(Router);
 
   private readonly productService = inject(ProductService);
 
+  protected readonly pageIndex = signal(1);
+
+  protected readonly pageSize = signal(2);
+
+  protected readonly totalCount = signal(0);
+
   protected readonly products = signal<Product[]>([]);
 
-  ngOnInit(): void {
-    this.products.set(this.productService.getList());
+  constructor() {
+    effect(() => {
+      const pageIndex = this.pageIndex();
+      const pageSize = this.pageSize();
+      this.getProducts(pageIndex, pageSize);
+    });
   }
 
   protected onEdit(product: Product): void {
@@ -28,5 +39,11 @@ export class ProductPageComponent implements OnInit {
 
   protected onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
+  }
+
+  private getProducts(pageIndex: number, pageSize: number): void {
+    const { data, count } = this.productService.getList(undefined, pageIndex, pageSize);
+    this.products.set(data);
+    this.totalCount.set(count);
   }
 }
