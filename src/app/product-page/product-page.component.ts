aa49beff3709b248de488a432db6c3d62e08473d
Oliver Huang <oliver721003@gmail.com>
2026-04-29 17:09:05 +0800

    feat(product-page): 新增產品功能實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -33,6 +33,20 @@ export class ProductPageComponent {
     });
   }
 
+  protected onAdd(): void {
+    const product = new Product({
+      name: '書籍 Z',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    });
+    this.productService.add(product);
+    this.getProducts(this.pageIndex(), this.pageSize());
+  }
+
   protected onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
   }
