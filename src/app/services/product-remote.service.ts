64ce530c918f4bbb75ec2c346dacb7919b6bfaeb
Oliver Huang <oliver721003@gmail.com>
2026-05-13 16:26:02 +0800

    feat(product): 實作從後端分頁取得產品資料

--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 
 import { ProductService } from './product.service';
@@ -14,6 +14,9 @@ export class ProductRemoteService extends ProductService  {
   private readonly httpClient = inject(HttpClient);
 
   override getList(name: string | undefined, index: number, size: number): Observable<{ data: Product[]; count: number }> {
-    return this.httpClient.get<Product[]>(this.url).pipe(map((data) => ({ data, count: data.length })));
+    const params = new HttpParams({ fromObject: { _page: index, _per_page: size } });
+    return this.httpClient
+      .get<{ data: Product[]; items: number }>(this.url, { params })
+      .pipe(map(({ data, items: count }) => ({ data, count })));
   }
 }
